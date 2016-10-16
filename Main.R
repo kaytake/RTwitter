9a3d059bc73947ab9f0105c4f6b8ae9a840fb6b4
Kei Takeda <kei.takeda@gmail.com>
2016-10-16 18:46:43 +0900

    list と vectorの違いがわかってきた！

--- Main.R
+++ Main.R
@@ -12,31 +12,11 @@ unique(df$text)
 str(df)
 is.vector(df[,1])
 
-p = paste(df$text, collapse = " ") #全部のtweetを連結
-mecabed = RMeCabC(p) #形態素解析
-p
-mecabed
-str(mecabed)
-table()
+p <- paste(df$text, collapse = " ") #全部のtweetを連結
+mecabed <- RMeCabC(p) #形態素解析
+lmecabed <- unlist(mecabed)
+summary(lmecabed)
 
-mecabed <- rep(1,length=1)
-  
-tb <- c(mecabed,rep(1,length=1))
+tb <- table(lmecabed)
+summary(tb)  
 tb
-a<- data.frame(Word=mecabed, Count=numeric(1))
-str(a)
-head(a)
-
-table(iris$Species)
-x=1:5
-x
-
-b= data.frame(a)
-str(b)
-
-summary(a)
-class(mecabed)
-
-mecabed[,names=="名詞"]
-head(mecabed)
-str(mecabed)
