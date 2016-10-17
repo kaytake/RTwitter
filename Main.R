a3ea63669c92cf3c945271aac8c6efeee35a2ccf
Kei Takeda <kei.takeda@gmail.com>
2016-10-17 20:07:35 +0900

    tweet ごとの頻出単語が取れた

--- Main.R
+++ Main.R
@@ -2,21 +2,30 @@ source("Util.R")
 
 twitterInit()
 
-df = searchTweet("マウンテンビュー")
-df = searchTweet("武蔵小杉", n = 10)
-str(df)
-summary(df)
-head(df)
-unique(df$text)
+df = searchTweet("PSVR")
+df = searchTweet("東京オリンピック", n=1000)
+df = searchTweet("日吉", n=500)
+df = searchTweet("田園調布", n=500)
 
-str(df)
-is.vector(df[,1])
-
+tmpFileName <- "tmpTweet.txt"
 p <- paste(df$text, collapse = " ") #全部のtweetを連結
-mecabed <- RMeCabC(p) #形態素解析
-lmecabed <- unlist(mecabed)
-summary(lmecabed)
+p
+cat(p, file=tmpFileName)
+#mecabed <- RMeCabC(p) #形態素解析 - 量が多いとクラッシュ？ファイルに一旦書くか。
+mecabed <- RMeCabFreq(tmpFileName)
+str(mecabed)
 
-tb <- table(lmecabed)
-summary(tb)  
-tb
+sortlist <- order(mecabed$Freq,decreasing = TRUE) #出現順にソート
+orderedResult <- mecabed[sortlist,]
+
+eliminateResult <- orderedResult[(orderedResult$Info2=="固有名詞"),]
+eliminateResult <- orderedResult[(orderedResult$Info1=="形容詞"),]
+eliminateResult <- orderedResult[(orderedResult$Info1=="動詞"),]
+eliminateResult <- orderedResult[(orderedResult$Info1=="副詞"),]
+
+head(eliminateResult,20)
+
+head(orderedResult,100)
+
+install.packages("wordcloud")
+library(wordcloud)
